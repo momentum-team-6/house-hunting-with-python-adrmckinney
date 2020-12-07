bb65c8c41190ddf904e62f30221f6cda006c69c5
Daniel McKinney <adrmckinney@gmail.com>
2020-12-07 17:46:23 -0500

    All the operations are working but the numbers aren't matching the example on the assignment so I assume something inside my conversions is not accurate.

--- house_hunting.py
+++ house_hunting.py
@@ -1,24 +1,28 @@
-# Write your code here
+
+# User Input
+annual_salary = float(input('Enter your annual salary: '))
+portion_saved = float(input('Enter the percent of your salary to save, as a decimal: '))
+total_cost = float(input('Enter the cost of your dream home: '))
+
+# conversions
+monthly_salary = annual_salary / 12
+print('monthly salary is', monthly_salary)
 
 portion_down_payment = .25
-current_savings = 10000
+
 r = .04
-monthly_return_rate = current_savings * r / 12
 
-# portion_saved = .10
-# portion_saved_per_month = monthly_salary * portion_saved
-# monthly_savings_increase = monthly_return_rate + portion_saved_per_month
-# future_savings = current_savings + monthly_savings_increase
-# number_of_months = 
+down_payment_cost = total_cost - (total_cost * portion_down_payment)
 
-# print(future_savings)
+adjusted_savings = (monthly_salary * portion_saved * r) + (portion_saved * monthly_salary)
 
-annual_salary = input('Enter your annual salary: ')
-portin_saved = input('Enter the percent of your salary to save, as a decimal: ')
-total_cost = input('Enter the cost of your dream home: ')
-# print('Number of months:', number_of_months)
+# Loop
+current_savings = 0
+number_of_months = 0
 
-monthly_salary = annual_salary / 12
+while current_savings < down_payment_cost:
+    current_savings += adjusted_savings
+    number_of_months += 1
 
-print(annual_salary)
-print('monthly salory is: ', monthly_salary)
+print('Number of months: ', number_of_months) 
+print('Savings amount ', current_savings)
