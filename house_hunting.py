86ca90fa4f90b6f5eaabb9e34cdd6c13d7e87a46
Daniel McKinney <adrmckinney@gmail.com>
2020-12-07 14:08:32 -0500

    started the homework, using input() and stuff

--- house_hunting.py
+++ house_hunting.py
@@ -1,1 +1,24 @@
 # Write your code here
+
+portion_down_payment = .25
+current_savings = 10000
+r = .04
+monthly_return_rate = current_savings * r / 12
+
+# portion_saved = .10
+# portion_saved_per_month = monthly_salary * portion_saved
+# monthly_savings_increase = monthly_return_rate + portion_saved_per_month
+# future_savings = current_savings + monthly_savings_increase
+# number_of_months = 
+
+# print(future_savings)
+
+annual_salary = input('Enter your annual salary: ')
+portin_saved = input('Enter the percent of your salary to save, as a decimal: ')
+total_cost = input('Enter the cost of your dream home: ')
+# print('Number of months:', number_of_months)
+
+monthly_salary = annual_salary / 12
+
+print(annual_salary)
+print('monthly salory is: ', monthly_salary)
